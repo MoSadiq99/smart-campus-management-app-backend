3859e087b93e8b1c50a1f1409d5ec4eafc39f0c3
Sadiq Naseer <mail2sdq99@gmail.com>
2025-02-18 13:14:53 +0530

    Initial commit

---
+++ src/test/java/edu/kingston/smartcampus/SmartcampusApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kingston.smartcampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcampusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
