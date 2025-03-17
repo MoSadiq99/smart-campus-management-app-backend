5bf9afbcf724dc9f6312f26962ec8046c3432b14
DunuraDulshan <ddulshan100@gmail.com>
2025-03-17 22:11:26 +0530

    Swagger Fix: Refactor WebMvcConfig to implement WebMvcConfigurer

--- src/main/java/edu/kingston/smartcampus/config/WebMvcConfig.java
+++ src/main/java/edu/kingston/smartcampus/config/WebMvcConfig.java
@@ -6,12 +6,12 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
-public class WebMvcConfig extends WebMvcConfigurationSupport {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
@@ -21,6 +21,5 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         converter.setObjectMapper(mapper);
         converters.add(converter);
-        super.configureMessageConverters(converters);
     }
 }
